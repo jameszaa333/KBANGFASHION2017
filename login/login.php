9fe23f6d71b155154df97734aed5c1128ea071fd
Izener <aoy_1130@hotmail.com>
2017-12-04 16:21:21 +0700

    howto use heidi sql @_@

---
+++ login/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css"href="style.css">
+  </head>
+
+  <body>
+    <div id="frm">
+      <form action="process.php" method="post">
+        <p>
+          <lebel>Username</lebel>
+          <input type="text" id="user"name="user" />
+        </p>
+
+        <p>
+          <lebel>Password</lebel>
+          <input type="password" id="pass "name="pass"  />
+        </p>
+        
+        <p>
+          <lebel>Password</lebel>
+          <input type="submit" id="btn" value="Login"  />
+        </p>
+
+      </form>
+
+    </div>
+
+  </body>
+</html>
