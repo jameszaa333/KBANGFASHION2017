ab6b601b886bd15c8696258b1d336b510e3a061b
wannika40 <32494184+wannika40@users.noreply.github.com>
2017-12-06 08:25:09 +0700

    ทำไม่เป็นเฟ้ย..

--- modelsession.php
+++ modelsession.php
@@ -11,8 +11,13 @@ $_SESSION["modelN2"] = "/assets/images/model/modelN2.png";
 $_SESSION["modelN3"] = "/assets/images/model/modelN3.png";
 $_SESSION["modelN4"] = "/assets/images/model/modelN4.png";
 
+isset($modelN0, $modelN1, $modelN2, $modelN3, $modelN4);
 
-
+if (isset($_POST["modelN0"])) {
+    echo "Yes, mail is set";    
+}else{  
+    echo "N0, mail is not set";
+}
 ?>
 
 
